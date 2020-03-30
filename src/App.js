74f9868863f6638f81cefc07c8d796c95da963ca
lidorva93 <59537123+lidorva93@users.noreply.github.com>
2020-03-30 22:12:15 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FlipC from './components/flipCard';
+
+
+function App() {
+  return (
+    <div>
+      <FlipC/>
+      <p>Lidor</p>
+    </div>
+  );
+}
+
+export default App;
